beb8c064a827c6f3c4e53936b150df4f56d8f1ae
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2021-08-09 18:15:25 +0800

    remove unnecessary code

--- memory_profiler/examples/example_undecorated.py
+++
@@ -1,9 +0,0 @@
-
-def another_func():
-    """Undecorated function that allocates memory"""
-    c = [1] * (10 ** 6)
-    d = [1] * (10 ** 7)
-    return c, d
-
-if __name__ == '__main__':
-    another_func()
