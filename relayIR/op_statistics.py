de641e507c47123f8b53f9cbb0e7726001dd2697
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2021-08-27 19:45:53 +0800

    bad

--- relayIR/op_statistics.py
+++ relayIR/op_statistics.py
@@ -1,3 +1,5 @@
+import json
+
 op_time_collection = {}
 
 op_time_distribution = {}
@@ -5,10 +7,10 @@ op_time_distribution = {}
 def put_op_time(name, time):
     global op_time_collection
     total_op_time = 0.0
-    if op_time_collection.has_key("total_op_time"):
+    if "total_op_time" in op_time_collection:
         total_op_time = op_time_collection.get("total_op_time")
     accumulated_time = 0.0
-    if op_time_collection.has_key(name):
+    if name in op_time_collection:
         accumulated_time = op_time_collection.get(name)
     accumulated_time += time
     total_op_time += time
@@ -17,6 +19,7 @@ def put_op_time(name, time):
 
 def calculate_op_distribution(model_name):
     op_time_distribution["model_name"] = model_name
-    for k, v in op_time_collection:
+    for k in op_time_collection:
         if k != "total_op_time":
             op_time_distribution[k] = "%.2f%%" % (op_time_collection[k]/op_time_collection["total_op_time"] * 100)
+    return json.dumps(op_time_distribution), json.dumps(op_time_collection)
