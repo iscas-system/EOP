aa40a34446b1592603537cbdba8434dc728e8d9a
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2021-09-02 18:11:29 +0800

    bad

--- test/test_copy_op.py
+++ test/test_copy_op.py
@@ -7,11 +7,25 @@ from tvm import relay
 from tvm.contrib import graph_runtime
 from cnn_workload_generator import get_network, compile_without_log, create_graph_executor_on_single_device, evaluate_time_with_tvm_evaluator, create_operator_executor_on_single_device
 
+'''
+op time (ms) different batch size of data copy for (3, 224, 224):
+1:0.019,
+2:0.082,
+4:0.218,
+8:0.447,
+16:1.386,
+32:3.757,
+64:7.672,
+128:15.700115,
+256:31.655,
+512:62.228,
+1024:126.593,
+'''
 def test_copy(data, src_dev=tvm.cpu(0), dst_dev = tvm.cuda(0)):
     res = tvm.relay.device_copy(data, src_dev, dst_dev)
     return res
 
-batch_size = 1
+batch_size = 1024
 image_shape = (batch_size, 3, 224, 224)
 expr_var = tvm.relay.var("data", shape=image_shape, dtype="float32")
 output = test_copy(expr_var)
@@ -35,6 +49,6 @@ device = tvm.cpu(0)
 
 lib = compile_without_log(mod, target, params)
 module = create_graph_executor_on_single_device(lib,image_shape,target)
-module.run()
-print(module.get_input(0))
-# print(evaluate_time_with_tvm_evaluator(module, device))
+# module.run()
+# print(module.get_output(0))
+print(evaluate_time_with_tvm_evaluator(module, device))
