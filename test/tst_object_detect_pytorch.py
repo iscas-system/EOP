d534348dd88b10042cf3b6bc2c98b47358f0ae25
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2021-09-03 18:33:55 +0800

    for pytorch 1.7

---
+++ test/tst_object_detect_pytorch.py
@@ -0,0 +1,66 @@
+import tvm
+from tvm import relay
+from tvm import relay
+from tvm.runtime.vm import VirtualMachine
+from tvm.contrib.download import download_testdata
+
+import numpy as np
+import cv2
+
+# PyTorch imports
+import torch
+import torchvision
+
+in_size = 300
+
+input_shape = (1, 3, in_size, in_size)
+
+
+def do_trace(model, inp):
+    model_trace = torch.jit.trace(model, inp)
+    model_trace.eval()
+    return model_trace
+
+
+def dict_to_tuple(out_dict):
+    if "masks" in out_dict.keys():
+        return out_dict["boxes"], out_dict["scores"], out_dict["labels"], out_dict["masks"]
+    return out_dict["boxes"], out_dict["scores"], out_dict["labels"]
+
+
+class TraceWrapper(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, inp):
+        out = self.model(inp)
+        return dict_to_tuple(out[0])
+
+
+model_func = torchvision.models.detection.maskrcnn_resnet50_fpn
+model = TraceWrapper(model_func(pretrained=True))
+
+model.eval()
+inp = torch.Tensor(np.random.uniform(0.0, 250.0, size=(1, 3, in_size, in_size)))
+
+with torch.no_grad():
+    out = model(inp)
+    script_module = do_trace(model, inp)
+
+img_url = (
+    "https://raw.githubusercontent.com/dmlc/web-data/" "master/gluoncv/detection/street_small.jpg"
+)
+img_path = download_testdata(img_url, "test_street_small.jpg", module="data")
+
+img = cv2.imread(img_path).astype("float32")
+img = cv2.resize(img, (in_size, in_size))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = np.transpose(img / 255.0, [2, 0, 1])
+img = np.expand_dims(img, axis=0)
+
+input_name = "input0"
+shape_list = [(input_name, input_shape)]
+mod, params = relay.frontend.from_pytorch(script_module, shape_list)
+
+print(mod)
