02fd22a7b82b8722006898f4fb264e52351acb46
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2021-12-09 20:33:17 +0800

    bad

--- op_time_profiler/RunPass.py
+++ op_time_profiler/RunPass.py
@@ -7,41 +7,51 @@ import tvm.testing
 import numpy as np
 from tvm.contrib import graph_executor
 import time
+from cnn_workload_generator import get_network,compile_with_executor,evaluate_time_with_tvm_evaluator
 
-dshape = (1, 16, 64, 64)
-x = relay.var("x", shape=dshape)
-x = relay.add(x, relay.const(1, "float32"))
-y = relay.nn.conv2d(x, relay.var("w1"), kernel_size=(1, 1), padding=(0, 0), channels=16)
-y1 = relay.add(relay.const(1, "float32"), y)
-y = relay.add(y, y1)
-z2 = relay.nn.conv2d(y, relay.var("w2"), kernel_size=(1, 1), padding=(0, 0), channels=16)
-z3 = relay.nn.conv2d(y, relay.var("w3"), kernel_size=(1, 1), padding=(0, 0), channels=16)
-z = relay.add(z2, z3)
-func = relay.Function(relay.analysis.free_vars(z), z)
-# print("before pass: ", func)
-# func = run_opt_pass(func, transform.FuseOps(fuse_opt_level=2))
-# print("after pass: ", func)
+# dshape = (1, 16, 64, 64)
+# x = relay.var("x", shape=dshape)
+# x = relay.add(x, relay.const(1, "float32"))
+# y = relay.nn.conv2d(x, relay.var("w1"), kernel_size=(1, 1), padding=(0, 0), channels=16)
+# y1 = relay.add(relay.const(1, "float32"), y)
+# y = relay.add(y, y1)
+# z2 = relay.nn.conv2d(y, relay.var("w2"), kernel_size=(1, 1), padding=(0, 0), channels=16)
+# z3 = relay.nn.conv2d(y, relay.var("w3"), kernel_size=(1, 1), padding=(0, 0), channels=16)
+# z = relay.add(z2, z3)
+# func = relay.Function(relay.analysis.free_vars(z), z)
+# # print("before pass: ", func)
+# # func = run_opt_pass(func, transform.FuseOps(fuse_opt_level=2))
+# # print("after pass: ", func)
+
+# mod = tvm.IRModule()
+# mod["main"] = func
+# mod = relay.transform.InferType()(mod)
+
+# model_params = {}
+# model_params["w1"] = np.random.uniform(0,10,(16, 16, 1, 1)).astype("float32")
+# model_params["w2"] = np.random.uniform(0,10,(16, 16, 1, 1)).astype("float32")
+# model_params["w3"] = np.random.uniform(0,10,(16, 16, 1, 1)).astype("float32")
+# x = np.random.uniform(5,10,(1, 16, 64, 64)).astype("float32")
+
+# with tvm.transform.PassContext(opt_level=3, disabled_pass=["AlterOpLayout"]):
+#    lib = relay.build(mod, target=target, params=model_params)
+#    interpreter = relay.build_module.create_executor("graph", lib.ir_mod, device, target)
+
 target = "llvm"
 dtype = "float32"
 device = tvm.cpu()
-mod = tvm.IRModule()
-mod["main"] = func
-mod = relay.transform.InferType()(mod)
 
-model_params = {}
-model_params["w1"] = np.random.uniform(0,10,(16, 16, 1, 1)).astype("float32")
-model_params["w2"] = np.random.uniform(0,10,(16, 16, 1, 1)).astype("float32")
-model_params["w3"] = np.random.uniform(0,10,(16, 16, 1, 1)).astype("float32")
-x = np.random.uniform(5,10,(1, 16, 64, 64)).astype("float32")
+mod, params, input_shape, output_shape = get_network("resnet-50", 1)
+x = np.random.uniform(5,10,input_shape).astype("float32")
+lib,interpreter,module = compile_with_executor(mod,device,target,params,3, [x])
+evaluate_time_with_tvm_evaluator(module,device)
 
-with tvm.transform.PassContext(opt_level=3, disabled_pass=["AlterOpLayout"]):
-   lib = relay.build(mod, target=target, params=model_params)
-   interpreter = relay.build_module.create_executor("graph", lib.ir_mod, device, target)
-
-t1 = time.time_ns()
-top1_tvm = interpreter.evaluate()(tvm.nd.array(x.astype(dtype)), **model_params)
-t2 = time.time_ns() - t1
-print(t2/1000)
+print(type(interpreter))
+print(type(module))
+# t1 = time.time_ns()
+# top1_tvm = interpreter.evaluate()(tvm.nd.array(x.astype(dtype)), **params)
+# t2 = time.time_ns() - t1
+# print(t2/1e9)
 
 # dev = tvm.device(target, 0)
 # m = graph_executor.GraphModule(lib["default"](dev))
--- relayIR/cnn_workload_generator.py
+++ relayIR/cnn_workload_generator.py
@@ -73,6 +73,15 @@ def compile_without_log(mod, target, params):
         lib = relay.build(mod, target=target, params=params)
     return lib
 
+def compile_with_executor(mod, device, target, params, level, input_args):
+    with tvm.transform.PassContext(opt_level=level):
+        lib = relay.build(mod, target=target, params=params)
+        interpreter = relay.build_module.create_executor("graph", lib.ir_mod, device, target)
+        module = graph_executor.GraphModule(lib["default"](device))
+    for i in range(len(input_args)):
+        module.set_input(i, input_args[i])
+    return lib,interpreter,module
+
 def create_graph_executor_on_single_device(lib,input_shape,target,dtype="float32"):
     dev = tvm.device(str(target), 0)
     module = graph_executor.GraphModule(lib["default"](dev))
@@ -88,7 +97,7 @@ def create_operator_executor_on_single_device(lib, input_args, target):
     return module
 
 def evaluate_time_with_tvm_evaluator(module, dev):
-    ftimer = module.module.time_evaluator("run", dev, repeat=1, min_repeat_ms=500, number=1)
+    ftimer = module.module.time_evaluator("run", dev, repeat=4, min_repeat_ms=500, number=1)
     prof_res = np.array(ftimer().results) * 1e3* 1e6  # convert to millisecond
     print("Mean inference time (std dev): %f ns (%f ns)" % (np.mean(prof_res), np.std(prof_res)))
     return np.mean(prof_res), np.std(prof_res)
